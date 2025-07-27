924f56bcf66b441cbafc59c467eed5a2726b9178
Madhusudan Mahatha <madhusudanmahatha14@gmail.com>
2025-07-27 07:53:58 +0000

    make the fade in transtion

--- src/components/layout/PageTransition.tsx
+++ src/components/layout/PageTransition.tsx
@@ -12,9 +12,9 @@ const PageTransition = ({ children }: { children: ReactNode }) => {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 15 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 15 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 0.3, ease: 'easeInOut' }}
       >
         {children}
