d7c30dc4e9f2f9b2eafe8174abb79b5c3718d73f
Madhusudan Mahatha <madhusudanmahatha14@gmail.com>
2025-07-27 07:57:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/PageTransition.tsx
+++ src/components/layout/PageTransition.tsx
@@ -14,7 +14,6 @@ const PageTransition = ({ children }: { children: ReactNode }) => {
         key={pathname}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
         transition={{ duration: 0.3, ease: 'easeInOut' }}
       >
         {children}
