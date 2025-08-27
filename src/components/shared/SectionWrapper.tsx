dec02a6f66da1792bb48cbec344fef5cf01d4ceb
Madhusudan Mahatha <madhusudanmahatha14@gmail.com>
2025-08-27 11:43:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HeaderNav>
    
    with

--- src/components/shared/SectionWrapper.tsx
+++ src/components/shared/SectionWrapper.tsx
@@ -13,7 +13,7 @@ export default function SectionWrapper({ title, children, className }: SectionWr
       <h2 className="text-3xl md:text-4xl font-bold mb-2 text-foreground relative inline-block">
         {title}
         {/* This span is for the accent line below the title */}
-        <span className="absolute bottom-[-8px] left-0 w-2/3 h-1 bg-accent rounded-full"></span>
+        <span className="absolute bottom-[-8px] left-0 w-24 h-1 bg-accent rounded-full"></span>
       </h2>
       <div className="mt-8">
         {children}
