49c6a192c3dabaa36a6f6de7117db8807125bd51
Madhusudan Mahatha <madhusudanmahatha14@gmail.com>
2025-08-27 10:00:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/data/profileData.ts
+++ src/data/profileData.ts
@@ -75,7 +75,7 @@ export const profileData: ProfileData = {
         "Smart fetching strategies bypass common hurdles like CORS.",
         "A next-generation tool for developers, designers, and SEO analysts."
       ],
-      image: "https://placehold.co/600/400.png",
+      image: "https://images.unsplash.com/photo-1669023414166-a4cc7c0fe1f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxOXx8d2Vic2l0ZSUyMGJ1aWxkZXJ8ZW58MHx8fHwxNzU2Mjg4NzkyfDA&ixlib=rb-4.1.0&q=80&w=1080",
       dataAiHint: "website analytics",
       link: "https://github.com/msmahatha/Website-Creator.git",
       liveLink: "https://websitecreator007.netlify.app/"
