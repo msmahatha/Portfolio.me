27b868b67559d6f1e2bf382ce5a3df13cb98e5a6
Madhusudan Mahatha <madhusudanmahatha14@gmail.com>
2025-09-03 18:16:47 +0000

    THE DAY THEME IS PERSISTING

--- src/components/layout/ThemeSwitcher.tsx
+++ src/components/layout/ThemeSwitcher.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun, Palette } from "lucide-react"
+import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -26,22 +26,20 @@ export function ThemeSwitcher() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <Palette className="h-[1.2rem] w-[1.2rem]" />
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("day")}>
-          <Sun className="mr-2 h-4 w-4" />
-          <span>Day</span>
+          Day
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("evening")}>
-          <Moon className="mr-2 h-4 w-4" />
-          <span>Evening</span>
+          Evening
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          <Moon className="mr-2 h-4 w-4" />
-          <span>Dark</span>
+          Dark
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
