fa2848ab4569d44e07d446443a1b6d9e2e183465
Madhusudan Mahatha <madhusudanmahatha14@gmail.com>
2025-09-06 20:17:38 +0000

    just make the same symbol as leetcde one

--- src/components/shared/IconMap.tsx
+++ src/components/shared/IconMap.tsx
@@ -6,21 +6,8 @@ const LeetCodeIcon = (props: LucideProps) => (
   <Code2 {...props} />
 );
 
-const CodeforcesIcon = ({ className, ...props }: LucideProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path d="M4.5 14.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="#1F8ACB" stroke="none" />
-      <path d="M9.5 12a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="#1F8ACB" stroke="none" />
-      <path d="M14.5 9.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="#F44336" stroke="none" />
-    </svg>
+const CodeforcesIcon = (props: LucideProps) => (
+  <Code2 {...props} />
 );
 
 
