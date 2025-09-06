da4650516f820f6dc1679b53dfcb2a5e0f2525b4
Madhusudan Mahatha <madhusudanmahatha14@gmail.com>
2025-09-06 20:13:08 +0000

    fix the code forces logo

--- src/components/shared/IconMap.tsx
+++ src/components/shared/IconMap.tsx
@@ -7,7 +7,21 @@ const LeetCodeIcon = (props: LucideProps) => (
 );
 
 const CodeforcesIcon = (props: LucideProps) => (
-  <Terminal {...props} />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M4.5 14.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="currentColor" stroke="none" />
+      <path d="M9.5 12a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="currentColor" stroke="none" />
+      <path d="M14.5 12a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="currentColor" stroke="none" />
+      <path d="M19.5 12a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="#F44336" stroke="none" />
+    </svg>
 );
 
 
